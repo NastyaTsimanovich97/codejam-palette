36114f4c207f9ed578de92b9cbbe96e2dd17bbee
NastyaTsimanovich97 <nastya.cimanovich.97@gmail.com>
2019-11-08 16:48:29 +0300

    feat: implement active button and record in localStorage

--- palette/app.js
+++ palette/app.js
@@ -1,4 +1,5 @@
 import './scss/base.scss'
 import './js/canvas.js'
+import './js/activeButton.js'
 
 console.log('codejam-palette')
---
+++ palette/js/activeButton.js
@@ -0,0 +1,31 @@
+let activeButton;
+if(!localStorage.getItem('activeButton')){
+    activeButton = document.querySelector('.button__paint-bucket');
+    activeButton.classList.toggle("isActive__button");
+    localStorage.setItem('activeButton', 'button__paint-bucket')
+}else{
+    let classActive = localStorage.getItem('activeButton');
+    activeButton = document.querySelector("."+classActive);
+    activeButton.classList.toggle("isActive__button");
+}
+
+document.addEventListener('click',function(event){
+    if(event.target.id=='button__palette'){
+        activeButton.classList.remove("isActive__button");
+        activeButton = event.target;
+        activeButton.classList.toggle("isActive__button");
+        if(activeButton.classList.contains("button__choose-color")){
+            localStorage.setItem('activeButton', 'button__choose-color');
+        }
+        if(activeButton.classList.contains("button__pencil")){
+            localStorage.setItem('activeButton', 'button__pencil');
+        }
+        if(activeButton.classList.contains("button__transform")){
+            localStorage.setItem('activeButton', 'button__transform');
+        }
+        if(activeButton.classList.contains("button__paint-bucket")){
+            localStorage.setItem('activeButton', 'button__paint-bucket');
+        }
+    }
+})
+
